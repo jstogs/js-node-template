3c71d4061865041e0c25e38b324e046bc21754c0
Jessica Stogsdill <jessica.stogsdill@student.swic.edu>
2025-03-20 17:00:14 -0500

    Add string formatting code

---
+++ src/strings.js
@@ -0,0 +1,7 @@
+const name = 'Jessica Stogsdill'
+const course = 'CS 177'
+const grade = '75'
+const messageTraditional = name + ' is taking ' + course + ' and expects to earn ' + grade + '%.'
+console.log(messageTraditional)
+const messageTemplateLaterals = `${name} is taking ${course} and expects to earn ${grade}%.`
+console.log(messageTemplateLaterals)
